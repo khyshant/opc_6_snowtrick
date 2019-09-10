d306be271a14d41cce1f27e5a87a88bfb08d319b
khyshant <anth.blanchard@gmail.com>
2019-09-11 00:40:43 +0200

    first route
    First route by annotation
    add construct on HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,28 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class HomeController
+
+
+class HomeController extends AbstractController
 {
+    /*
+     * @ Environment
+     **/
+    private $twig;
 
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
     public function index(): Response
     {
         return new Response('hello');
